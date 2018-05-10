a3098b789ef36f3c5749d13a4937bcd56401614e
Randall Fulton <randall@shipt.com>
2018-05-10 15:28:42 -0500

    Install raw packages

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -6,6 +6,9 @@ cd /jupyter-init.d/
 if [ -f requirements.txt ]; then
     pip3 install -r requirements.txt
 fi
+for package in */; do
+    pip3 install ./$package
+done
 cd $START_DIR
 
 su -m $DOCKER_USER -c "jupyter lab --allow-root --ip=* --port=8888 --no-browser --notebook-dir=/notebooks"
